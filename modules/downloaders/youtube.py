301d0afa86b72af6f99d6112537618a4d9815931
SHEKHAR UPADHYAY <96115514+BlackVirusOfficial@users.noreply.github.com>
2022-04-30 08:59:43 +0530

    Add files via upload

---
+++ modules/downloaders/youtube.py
@@ -0,0 +1,30 @@
+from os import path
+
+from yt_dlp import YoutubeDL
+
+from modules.config import DURATION_LIMIT
+from modules.helpers.errors import DurationLimitError
+
+ydl_opts = {
+    "format": "bestaudio/best",
+    "geo-bypass": True,
+    "nocheckcertificate": True,
+    "outtmpl": "downloads/%(id)s.%(ext)s",
+}
+ydl = YoutubeDL(ydl_opts)
+
+
+def download(url: str) -> str:
+    info = ydl.extract_info(url, False)
+    duration = round(info["duration"] / 60)
+    if duration > DURATION_LIMIT:
+        raise DurationLimitError(
+            f"🛑 Vɪᴅᴇᴏ ʟᴏɴɢᴇʀ ᴛʜᴀɴ {DURATION_LIMIT} ᴍɪɴᴜᴛᴇ(s) Aʀᴇɴ'ᴛ ,ᴀʟʟᴏᴡᴅᴇᴅ ᴛʜᴇ ᴘʀᴏᴠɪᴅᴇ ᴠɪᴅᴇᴏ ɪs {duration} ᴍɪɴᴜᴛᴇ(s)",
+        )
+    try:
+        ydl.download([url])
+    except:
+        raise DurationLimitError(
+            f"🛑 Vɪᴅᴇᴏ ʟᴏɴɢᴇʀ ᴛʜᴀɴ {DURATION_LIMIT} ᴍɪɴᴜᴛᴇ(s) Aʀᴇɴ'ᴛ ,ᴀʟʟᴏᴡᴅᴇᴅ ᴛʜᴇ ᴘʀᴏᴠɪᴅᴇ ᴠɪᴅᴇᴏ ɪs {duration} ᴍɪɴᴜᴛᴇ(s)",
+        )
+    return path.join("downloads", f"{info['id']}.{info['ext']}")
