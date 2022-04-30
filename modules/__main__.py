04482f21d1ed0e1e1a80f7d68517ae71adcbe842
NitricXd <92805675+NitricXd@users.noreply.github.com>
2022-04-30 18:50:38 +0530

    Update __main__.py

--- modules/__main__.py
+++ modules/__main__.py
@@ -1,6 +1,6 @@
 from pyrogram import idle
 from pyrogram import Client as Bot
-from modules.clientbot import run
+from modules.clientbot.clientbot import run
 from modules.config import API_ID, API_HASH, BOT_TOKEN
 
     
